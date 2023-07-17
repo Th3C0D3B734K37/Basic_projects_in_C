029b3e42c670a388de288a0623e7f7cc5e044ed5
Th3C0D3 B734K37 <109842778+Th3C0D3B734K37@users.noreply.github.com>
2023-07-17 18:26:02 +0530

    Create Basic_Calculator.c
    The code is a basic calculator program that performs arithmetic operations on two input numbers.
    The user is prompted to enter two values, which are stored in variables a and b.
    The user is then asked to choose an arithmetic operation (addition, subtraction, multiplication, or division) by entering a corresponding number.
    The program uses a for loop to check the user's input and perform the requested arithmetic operation.
    Inside the loop, the program checks the value of action and executes the corresponding operation.
    After performing the operation, the result is displayed on the screen.
    If the user enters an invalid value (not 1, 2, 3, or 4), an error message is displayed.
    The program uses variables (sum, sub, mul, divi) to store the results of each operation.

---
+++ Basic_Calculator.c
@@ -0,0 +1,62 @@
+/*    The code is a basic calculator program that performs arithmetic operations on two input numbers.
+      The user is prompted to enter two values, which are stored in variables a and b.
+      The user is then asked to choose an arithmetic operation (addition, subtraction, multiplication, or division)
+      by entering a corresponding number.   */
+
+#include<stdio.h>
+
+int main()
+{
+  int a , b , sum , sub , mul , divi;
+  int i;
+  
+  printf(" Enter two values of :-\n");
+  scanf(" %d %d",&a,&b);
+  printf("\n  a : %d \n  b : %d \n ",a,b);
+  
+  
+  printf("\n What action do you want to perform inside the calculator ?\n");
+  printf("\n Addition = 1 \n Subtraction = 2 \n Multiplication = 3 \n Division = 4 \n");
+  scanf("%d",&i);
+  
+  for(i ; i<= 4 ;++i)
+  {
+    if(i==1)
+    {
+        sum = a + b;
+        printf("\n So the Addition of 'a' and 'b' is:-> %d \n",sum);
+        break;
+    }
+    
+    else if(i==2)
+    
+    {
+       sub = a - b;
+       printf("\n So the subtraction of 'a' and 'b' is:-> %d \n",sub);
+       break; 
+    }
+    
+    else if(i==3)
+    
+    {
+         mul = a * b;
+         printf("\n So the multiplication of 'a' and 'b' is:-> %d \n",mul);
+         break;  
+    }
+    else if(i==4)
+    {
+        divi = a / b;
+        printf("\n So the division of 'a' and 'b' is:-> %d \n",divi);
+        break;
+    }
+    
+    else
+    {
+        printf("Invalid Input"); 
+        break;
+    }
+    
+  }
+  return 0;
+} 
+
